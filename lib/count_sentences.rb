a34d1ab2a1a97ec0b9dc51fbc218270ae3592a23
Wendy Calderon <wendycalderon@gmail.com>
2018-07-04 02:55:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+     counter = 0 
+     array = []
+     array = self.split (/[\.!?]+/)
+     counter = array.count 
   end
 end
