41fdc101d62829aac8121056368b63380b13b68f
Wendy Calderon <wendycalderon@gmail.com>
2018-07-04 02:25:33 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+     counter = 0 
+     self.end_with?(".") ? true : false
   end
 end
